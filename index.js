c2bb76a0084d9a5e348c1aef3baac2f51f2b06dd
Jen Weber <j@jenweber.me>
2019-04-19 18:07:14 -0400

    fix scss

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ module.exports = {
   name: require('./package').name,
   isDevelopingAddon() {
     return true
+  },
+  included: function(/* app */) {
+    this._super.included.apply(this, arguments);
   }
 };
