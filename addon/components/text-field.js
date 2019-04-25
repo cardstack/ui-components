865fe74bb85b35e13521d7ca43837a42f1ceb919
Derrick Camerino <robustdj@gmail.com>
2019-04-25 10:09:22 -0700

    Sass cleanup, add param for required/optional for text-field

--- addon/components/text-field.js
+++ addon/components/text-field.js
@@ -1,7 +1,12 @@
 import Component from '@ember/component';
 import layout from '../templates/components/text-field';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
-  classNames: 'text-field'
+  classNames: 'text-field',
+  required: false,
+  inputId: computed('elementId', function() {
+    return `${this.elementId}-input`;
+  })
 });
