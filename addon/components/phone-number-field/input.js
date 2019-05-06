203ea34b138d78b88904301106f0480aded04164
Derrick Camerino <robustdj@gmail.com>
2019-05-06 14:35:46 -0700

    Remove unneeded template

--- addon/components/phone-number-field/input.js
+++ addon/components/phone-number-field/input.js
@@ -1,8 +1,6 @@
 import OneWayPhoneMask from 'ember-inputmask/components/one-way-phone-mask';
-// import layout from '../../templates/components/phone-number-field/input';
 
 export default OneWayPhoneMask.extend({
-  // layout
   attributeBindings: [
     'type',
     '_value:value',
