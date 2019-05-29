b4a42ff1ba2f9888084139d6ffb835856e1da3f6
Derrick Camerino <robustdj@gmail.com>
2019-05-29 15:11:27 -0700

    FileUploadDialog component

---
+++ addon/components/file-upload-dialog.js
@@ -0,0 +1,25 @@
+import Component from '@ember/component';
+import layout from '../templates/components/file-upload-dialog';
+import { inject as service } from '@ember/service';
+import { A } from '@ember/array';
+
+export default Component.extend({
+  layout,
+  classNames: ['cs-component-file-upload-dialog'],
+  attributeBindings: ['dataTestName:data-test-cs-component-file-upload-dialog'],
+  dataTestName: true,
+  files: A([]),
+  fileQueue: service(),
+  actions: {
+    uploadFile(file) {
+      const now = new Date();
+      file.uploadedDate = `${now.getMonth()}.${now.getDay()}.${now.getFullYear()}`;
+      this.files.addObject(file);
+    },
+
+    removeFile(file) {
+      this.files.removeObject(file);
+    }
+  }
+
+});
---
+++ tests/integration/file-upload-dialog-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, fillIn, click, find, triggerEvent } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | file-upload-dialog', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders component', async function(assert) {
+    await render(hbs`<FileUploadDialog @acceptedFormats="png, gif, jpg" />`);
+
+    assert.dom('[data-test-cs-component-file-upload-dialog]').exists();
+    assert.dom('[data-test-cs-component-file-upload-dialog] [data-test-cs-component-file-upload-dialog-accepted-formats]').hasText('png, gif, jpg');
+  });
+
+  test('it shows file data after one has been uploaded', async function(assert) {
+    await render(hbs`<FileUploadDialog @acceptedFormats="txt, rtf, dat" />`);
+    let file = new File(["foo bar baz"], 'node.txt', { type: 'text/plain' });
+    await triggerEvent(find('[data-test-cs-component-file-upload-dialog] input'), 'change', [file] );
+    assert.dom('[data-test-cs-component-file]').exists();
+    assert.dom('[data-test-cs-component-file] h4').hasText('node.txt');
+
+    await click('[data-test-cs-component-remove-file]');
+    assert.dom('[data-test-cs-component-file]').doesNotExist();
+  });
+});
