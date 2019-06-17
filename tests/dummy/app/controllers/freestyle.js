f02972e14c2ba6427f9086a3ae0335971fa4f9b8
Derrick Camerino <robustdj@gmail.com>
2019-06-17 08:33:20 -0700

    Some cleanup, fix eslint errors

--- tests/dummy/app/controllers/freestyle.js
+++ tests/dummy/app/controllers/freestyle.js
@@ -13,9 +13,9 @@ const countries = [
   { name: 'United Kingdom', formalName: 'United Kingdom of Great Britain and Northern Ireland' }
 ];
 const customDropdownOptions = [
-  { imageUrl: 'https://via.placeholder.com/50x35',  cardType: 'Ether', transactionId: '0xCb3d...C16fc', network: 'Mainnet', fromValue: '15.3532 ETH', toValue: '$3993.7 USD' },
-  { imageUrl: 'https://via.placeholder.com/50x35',  cardType: 'Bitcoin', transactionId: '0xCb3a...C36fc', network: 'Rinkeby', fromValue: '2.3532 BTC', toValue: '$8509 USD' },
-  { imageUrl: 'https://via.placeholder.com/50x35',  cardType: 'Litecoin', transactionId: '0xCb3a...B96fa', network: 'Ropstein', fromValue: '82.92 LIT', toValue: '$2547 USD' }
+  { imageUrl: 'https://via.placeholder.com/50x35', cardType: 'Ether', transactionId: '0xCb3d...C16fc', network: 'Mainnet', fromValue: '15.3532 ETH', toValue: '$3993.7 USD' },
+  { imageUrl: 'https://via.placeholder.com/50x35', cardType: 'Bitcoin', transactionId: '0xCb3a...C36fc', network: 'Rinkeby', fromValue: '2.3532 BTC', toValue: '$8509 USD' },
+  { imageUrl: 'https://via.placeholder.com/50x35', cardType: 'Litecoin', transactionId: '0xCb3a...B96fa', network: 'Ropstein', fromValue: '82.92 LIT', toValue: '$2547 USD' }
 ];
 
 export default FreestyleController.extend({
