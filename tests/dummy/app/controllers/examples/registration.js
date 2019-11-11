7359709455ebfd9c22cdcd6b0c7504901205f8f9
Jen Weber <j@jenweber.me>
2019-11-11 14:56:39 -0500

    use one way binding for checkbox (#128)
    * use one way binding for checkbox
    
    * fix registration
    
    * add Octane example

--- tests/dummy/app/controllers/examples/registration.js
+++ tests/dummy/app/controllers/examples/registration.js
@@ -1,10 +1,14 @@
 import Controller from '@ember/controller';
 import { A } from '@ember/array';
-import { computed } from '@ember/object';
+import { computed, action } from '@ember/object';
 
 export default Controller.extend({
   sourceOptions: A([ 'Magazine', 'Radio', 'EmberConf', 'Other']),
   isSubmitDisabled: computed('emailValue', 'isEmailValid', 'passwordValue', 'isPasswordValid', 'isCheckboxChecked', function() {
     return !(!!this.emailValue && !this.isEmailInvalid && !!this.passwordValue && !this.isPasswordInvalid && this.isCheckboxChecked);
+  }),
+  
+  setChecked: action(function(propertyToSet, val) {
+    this.set(propertyToSet, val);
   })
 });
