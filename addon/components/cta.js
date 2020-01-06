76522f21100b877e6ca117ebb98af9d2fc307eb4
Burcu Noyan <burcunoyan@gmail.com>
2020-01-06 15:22:35 -0500

    Add todo for spinner button

--- addon/components/cta.js
+++ addon/components/cta.js
@@ -1,9 +1,5 @@
 import BaseComponent from './base-component';
-// import { tracked } from '@glimmer/tracking';
 
 export default class Cta extends BaseComponent {
-  // @tracked variant;
-  // @tracked disabled = false;
-
   handleClick() {}
 }
