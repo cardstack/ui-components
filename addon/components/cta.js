97143c34fe09c8b8637b3e82047389c4681e9fbd
Burcu Noyan <burcunoyan@gmail.com>
2020-01-07 11:10:22 -0500

    f

--- addon/components/cta.js
+++ addon/components/cta.js
@@ -1,9 +1,5 @@
 import BaseComponent from './base-component';
 
 export default class Cta extends BaseComponent {
-  constructor(...args) {
-    super(...args);
-
-    this.handleClick = this.args.handleClick || (() => {});
-  }
+  handleClick() {}
 }
